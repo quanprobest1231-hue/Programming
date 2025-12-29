ac8f8c2b1a4614cfde628f9a33275fd444bc5575
minh quan <quanprobest1231@gmail.com>
2025-12-29 22:15:57 +0700

    update file Hello branch git tool

--- Git_Tool/hello.cpp
+++ Git_Tool/hello.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!, QUAN"};
-
+    cout << "Message: Hello Xi" << endl;
     for (const string& word : msg)
     {
         cout << word << " ";
