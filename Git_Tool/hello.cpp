111d514680b6b937ae8f9da0e709b412324f870e
minh quan <quanprobest1231@gmail.com>
2025-12-29 22:37:16 +0700

    Commit file hello.cpp branch master

--- Git_Tool/hello.cpp
+++ Git_Tool/hello.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!, QUAN"};
-    cout << "Message: Hello Xi" << endl;
+    cout << "Message: Hello Xiiiiiiiiiiiii" << endl;
     for (const string& word : msg)
     {
         cout << word << " ";
